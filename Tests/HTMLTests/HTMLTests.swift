08cd794b6567c2c6cf319c5d50cace168cc3c4f7
Ken Mueller <kenmueller0@gmail.com>
2019-11-19 15:00:25 -0800

    better example test

--- Tests/HTMLTests/HTMLTests.swift
+++ Tests/HTMLTests/HTMLTests.swift
@@ -20,16 +20,34 @@ final class HTMLTests: XCTestCase {
 					HTMLElement.head
 						.child {
 							HTMLElement.title
-								.child("Ken's Homepage")
+								.child("My Homepage")
+						}
+						.child {
+							HTMLElement.meta
+								.name("description")
+								.content("This is my homepage!")
 						}
 				}
 				.child {
 					HTMLElement.body
+						.background("yellow")
 						.child {
-							h1WithBlueColor
-								.background("red", important: true)
+							HTMLElement.h1
+								.color("blue")
+								.textAlign("center")
+								.child("My Homepage")
 						}
-						.child { h1WithBlueColor }
+						.child {
+							HTMLElement.p
+								.color("red")
+								.paddingTop("20px")
+								.child("Hi! This is my homepage")
+						}
+						.child {
+							HTMLElement.img
+								.src("/my-image-destination.jpg")
+								.alt("My image")
+						}
 				}
 		})
 	}
